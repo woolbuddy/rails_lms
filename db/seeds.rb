ac36348cda335be44f390295d8cdfe68e331a558
Zach Woolboy <zachtwoolboy@gmail.com>
2019-05-28 20:52:52 -0600

    end of day

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+roles = ['teacher', 'ta', 'student']
+
+10.times do
+  course = Course.create(
+    name: Faker::ChuckNorris.fact
+  )
+
+  10.times do
+    user = User.create(
+      first_name: Faker::Beer.brand ,
+      last_name:  Faker::Beer.hop
+    )
+
+    Enrollment.create(
+      role: roles.sample,
+      course_id: course.id,
+      user_id: user.id
+    )
+  end
+end
+
+puts "Data Seeded"
